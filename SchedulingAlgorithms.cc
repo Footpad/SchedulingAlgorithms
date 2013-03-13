ea6b90f8908a1723258c617d0ac085f2ce627879
dam7633 <dam7633@rit.edu>
2013-03-13 17:15:21 -0400

    Adding some stuff!

--- SchedulingAlgorithms.cc
+++ SchedulingAlgorithms.cc
@@ -1,6 +1,10 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "RMAScheduler.h"
+#include "EDFScheduler.h"
+#include "SCTScheduler.h"
+
 int main(int argc, char *argv[]) {
 	std::cout << "Welcome to the QNX Momentics IDE" << std::endl;
 	return EXIT_SUCCESS;
